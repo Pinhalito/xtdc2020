f088ffd035d5d6ae9666dc9661a6e50521deffec
Pinhalito <pinhalito@gmail.com>
2020-12-07 00:41:27 -0300

    Create vai.sh

---
+++ vai.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#######################
+#    ^...^  `^...^´   #
+#   / o,o \ / O,O \   #
+#   |):::(| |):::(|   #
+# ====" "=====" "==== #
+#         TdC         #
+#      1998-2020      #
+#######################
+# Toca das Corujas
+# Códigos Binários, Funções de Onda e Teoria do Orbital Molecular Inc.
+# Unidade Barão Geraldo CX
+
+
+testa1(){
+echo um
+}
+
+testa2(){
+echo "cuidado pois esse script roda todas as funções marcadas"
+}
+
+
+testa3(){
+echo ${FUNCNAME[0]}
+}
+
+
+
+
+xtdc_up(){
+ls
+}
+
+xtdc_install(){
+testa1 && testa2 && testa3 && xtdc_up
+}
